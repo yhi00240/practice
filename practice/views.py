1b091614b568095ba2c43811f95240e228d8dc60
hyunMin11 <dpqms0123@gmail.com>
2017-02-01 14:21:12 +0900

    render 'practice_name' to html

--- practice/views.py
+++ practice/views.py
@@ -18,13 +18,13 @@ class PracticeViewSet(ViewSet):
     @detail_route(methods=['get'])
     def input_data(self, request, practice_name=None):
         template_name = 'practice/data_input.html'
-        return render(request, template_name)
+        return render(request, template_name, {'practice_name':practice_name})
 
     @detail_route(methods=['post'])
     def load_data(self, request, practice_name=None):
         practice = PRACTICE_NAME_TO_CLASS[practice_name]()
         practice.load_training_data()
-        return HttpResponse({'success': True, 'data': practice.training_data}, content_type='application/json')
+        return HttpResponse({'success': True, 'data': practice.training_data, 'practice_name':practice_name}, content_type='application/json')
 
     @detail_route(methods=['post'])
     def upload(self, request, practice_name=None):
@@ -36,13 +36,13 @@ class PracticeViewSet(ViewSet):
             destination.write(chunk)
         destination.close()
         template_name = 'practice/data_input.html'
-        return render(request, template_name)
+        return render(request, template_name, {'practice_name':practice_name})
 
     @detail_route(methods=['get'])
     def algorithm(self, request, practice_name=None):
         template_name = 'practice/set_algorithm.html'
         practice = PRACTICE_NAME_TO_CLASS[practice_name]()
-        return render(request, template_name, practice.get_algorithm_settings())
+        return render(request, template_name, practice.get_algorithm_settings(), {'practice_name':practice_name})
 
     @detail_route(methods=['post'])
     def save_algorithm(self, request, practice_name=None):
@@ -51,7 +51,7 @@ class PracticeViewSet(ViewSet):
         request.session['Optimizer'] = request.data['Optimizer']
         request.session['WeightInitialization'] = request.data['WeightInitialization']
         request.session['Dropout'] = request.data['Dropout']
-        return redirect('/practice/mnist/training/')
+        return redirect('/practice/' + practice_name + 'training/')
 
     @detail_route(methods=['get'])
     def training(self, request, practice_name=None):
@@ -63,7 +63,7 @@ class PracticeViewSet(ViewSet):
     def save_training(self, request, practice_name=None):
         request.session['rate'] = float(request.data['rate'])
         request.session['epoch'] = int(request.data['epoch'])
-        return redirect('/practice/mnist/training_mnist/')
+        return redirect('/practice/' + practice_name + '/training_mnist/')
 
     @detail_route(methods=['get'])
     def training_mnist(self, request, practice_name=None):
@@ -73,10 +73,10 @@ class PracticeViewSet(ViewSet):
         mnist.set_algorithm()
         mnist.set_training(request.session['rate'], request.session['epoch'])
         mnist.run()
-        return render(request, template_name, {'user_epoch': mnist.training_epochs})
+        return render(request, template_name, {'user_epoch': mnist.training_epochs, 'practice_name':practice_name})
 
     @detail_route(methods=['get'])
     def test(self, request, practice_name=None):
         # TODO for front: 알맞은 html template 개발되면 적용
         template_name = 'practice/data_input.html'
-        return render(request, template_name)
+        return render(request, template_name, {'practice_name':practice_name})
