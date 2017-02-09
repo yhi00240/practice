a745a9a77d4b70be70f1da09b7720864f411e11b
hyunMin11 <dpqms0123@gmail.com>
2017-02-09 23:43:45 +0900

    add comments to base.js

--- practice/static/practice/js/base.js
+++ practice/static/practice/js/base.js
@@ -1,16 +1,25 @@
+/**
+  * function for menu
+  * menu contaion mainmenu and submenu
+  * if user doesn't click mainmenu, submenu doesn't show
+*/
 $(document).ready( function() {
-	var $submenu = $('.submenu');
-	var $mainmenu = $('.mainmenu');
-	$submenu.hide();
-	$submenu.first().delay(400).slideDown(700);
-	$submenu.on('click','li', function() {
-		$submenu.siblings().find('li').removeClass('chosen');
-		$(this).addClass('chosen');
-	});
-	$mainmenu.on('click', 'li', function() {
-		$(this).next('.submenu').slideToggle().siblings('.submenu').slideUp();
-	});
-	$mainmenu.children('li:last-child').on('click', function() {
-		$mainmenu.fadeOut().delay(500).fadeIn();
-	});
+  var $submenu = $('.submenu');
+  var $mainmenu = $('.mainmenu');
+
+  // At first, submenus doesn't show
+  $submenu.hide();
+
+  // $submenu.first().delay(400).slideDown(700);
+
+  // if you click a mainmenu item, make this item's attr to chosen
+  $submenu.on('click','li', function() {
+    $submenu.siblings().find('li').removeClass('chosen');
+	$(this).addClass('chosen');
+  });
+
+  // if you click a mainmenu item, make the item's submenu to show
+  $mainmenu.on('click', 'li', function() {
+    $(this).next('.submenu').slideToggle().siblings('.submenu').slideUp();
+  });
 });
