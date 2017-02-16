d572cc5fdd8012a66cb34c2b25d6bf428f0d7728
sora <leesr7111@gmail.com>
2017-02-17 01:42:35 +0900

    Re-define tables of database. Each row of the table consists of a image and label matched with the image.

--- practice/models.py
+++ practice/models.py
@@ -1,7 +1,16 @@
 from django.db import models
-class InputData(models.Model):
-    name = models.CharField(max_length=10)
-    trainImages = models.BinaryField()
-    trainLabels = models.BinaryField()
-    testImages = models.BinaryField()
-    testLabels = models.BinaryField()
+# class InputData(models.Model):
+#     name = models.CharField(max_length=10)
+#     trainImages = models.BinaryField()
+#     trainLabels = models.BinaryField()
+#     testImages = models.BinaryField()
+#     testLabels = models.BinaryField()
+
+
+class TrainData(models.Model):
+    image = models.BinaryField()
+    label = models.IntegerField()
+
+class TestData(models.Model):
+    image = models.BinaryField()
+    label = models.IntegerField()
