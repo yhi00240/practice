78a970fbfe93b0da7ce33b317857f5887e58e57c
sora <leesr7111@gmail.com>
2017-02-19 14:40:08 +0900

    Remove unused codes.

--- practice/models.py
+++ practice/models.py
@@ -1,11 +1,4 @@
 from django.db import models
-# class InputData(models.Model):
-#     name = models.CharField(max_length=10)
-#     trainImages = models.BinaryField()
-#     trainLabels = models.BinaryField()
-#     testImages = models.BinaryField()
-#     testLabels = models.BinaryField()
-
 
 class TrainData(models.Model):
     image = models.BinaryField()
